c5c8e69bca5641e66c39951e9153582d2f50bb8d
emanu <emanu@EMANUEL_RAMREZ>
2026-03-05 23:21:56 -0600

    Programa Creado!

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Escriba un programa que pida números al usuario hasta ingresar 0 y al final muestre:
+
+//la suma total de los números
+
+//cuántos números se ingresaron
+
+//el promedio de los números ingresados.
+
